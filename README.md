0520e2e46d87c1f7c77d43ab466f681b4457b620
Alex Stewart <sgtsmall@gmail.com>
2020-04-12 10:15:47 +1000

    Get things started

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a quick git of some sample work in ansible that I use to manage Raspberry Pi's and other linux servers.
+
+It's veracity is not warranted! I threw together parts of my working directory in a hurry to publish this sample.
+
+I will try to clean things up over time, however I believe several of these things work at this time.
+
+you will need to make up your own appropriate ssh key to be sent to clients for access.
+and install passwords for wifi as appropriate in the wpa_config
---
+++ alexpi.sh
@@ -0,0 +1,12 @@
+#ansible-playbook  playbooks/new-sdcard.yml
+#ansible-playbook  playbooks/new-defwifi.yml
+#ansible-playbook -vv playbooks/setup-epaper.yml
+#ansible-playbook -vv playbooks/setup-fldigi.yml
+#ansible-playbook -vv playbooks/setup-wsjtx.yml
+#ansible-playbook  playbooks/new-pi.yml
+
+#ansible-playbook  playbooks/new-default.yml
+#ansible-playbook -vv playbooks/setup-octo35.yml
+#ansible-playbook  playbooks/new-pinetclone.yml
+#ansible-playbook  playbooks/upd-pinetclone.yml
+ansible-playbook  playbooks/update-default.yml
